c86d9215598ec1cf2cfbe12c229cc9a24d42a56f
tavolafourcade <tavolafourcade@gmail.com>
2021-01-07 21:11:15 -0500

    Add absolute route for getting the Path using resolve

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let express = require('express');
 
 const app = express();
+const path = require('path');
 
 //console.log(app)
 
@@ -8,7 +9,29 @@ app.listen(3000, ()=>{
   console.log('Servidor corriendo')
 });
 
-//Especificando el siste de rooteo de express
-app.get('/', function(req, res){
-  res.send('Bienvenidos al sitio');
+//Especificando el sistema de rooteo de express
+// app.get('/', function(req, res){
+//   res.send('Bienvenidos al sitio');
+// })
+
+// app.get('/', function(){
+
+// })
+app.get('/contact', function(req,res){
+  res.send('Welcome to the contact section');
+})
+
+app.get('/un-array', function(req, res){
+  res.send([1,2,3,4])
+})
+
+app.get('/un-objeto', function(req,res){
+  res.send({name:'Octavio'})
+})
+
+app.get('/', (req, res)=>{
+  //resolve da una ruta absoluta.
+  let htmlPath= path.resolve(__dirname, './views/index.html');
+  //console.log(htmlPath)
+  res.sendFile(htmlPath)
 })
